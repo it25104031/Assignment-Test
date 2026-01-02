b2574425498190a38996d00e63af9318c17ec5b0
it25104031 <it25104031@my.sliit.lk>
2026-01-02 18:38:17 +0530

    spynet.c

---
+++ spynet.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//game constants 
+
+#define MIN_SIZE 5
+#define MAX_SIZE 15
+
+#define PLAYER_SYMBOL 	'@'
+#define WALL_SYMBOL 	'#'
+#define INTEL_SYMBOL	'I'
+#define LIFE_SYMBOL	'L'
+#define EXTRACT_SYMBOL	'X'
+#define EMPTY_SYMBOL 	'.' //Empty cell
+			   
+
