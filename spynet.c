b1ff4544293d848ebaba947afda602440ba5eb67
it25104031 <it25104031@my.sliit.lk>
2026-01-10 20:27:52 +0530

    spynet.c

--- spynet.c
+++ spynet.c
@@ -1,4 +1,5 @@
-
+#ifndef SPYNET_H
+#define SPYNET_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,17 +17,32 @@
 #define EXTRACT_SYMBOL	'X'
 #define EMPTY_SYMBOL 	'.' //Empty cell
 	
-//for player
+typedef struct{
 int row;
 int col;
 int lives;
 int intel_collected;
 int active; //1 = active , 0= inactive
+}Player;
 
-//for grid
+typedef struct{
 char **grid	//Dynamically allocated 2D grid
 int size	//grid dimention
 player p1;
 int total_intel;
+} Game;
 
+void init_game(Game *g, int size);
+void allocate_grid(Game *g);
+void place_items(Game *g);
 
+void display_map(struct Game *g);
+void log_move(struct Game *g, char move, const char *message);
+
+void process_move(struct Game *g, char direction);
+int is_valid_move(struct Game *g, int r, int c);
+int check_win_condition(struct Game *g);
+
+void free_memory(struct Game *g);
+
+#endif
